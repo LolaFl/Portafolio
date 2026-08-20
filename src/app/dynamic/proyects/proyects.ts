2275090fd83f4fb9ad4de0358c3ab46be9bea27a
Angeles Pietra <angeles10pietra@gmail.com>
2026-08-20 19:48:14 -0300

    agregue: metodo para cargar proyectos y metodo para crear proyecto en proyects.ts

--- src/app/dynamic/proyects/proyects.ts
+++ src/app/dynamic/proyects/proyects.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common'; 
 import { proyectService } from '../../services/proyects';
+import { CreateProyectComponent } from '../create-proyect/create-proyect';
 
 export interface Proyecto 
 {
@@ -11,7 +13,7 @@ export interface Proyecto
   estado_del_proyecto: string;
   icono: string;
   tecnologias: string[];
-  rol_del_equipo: string; 
+  rol_del_equipo: string;
   repo: string;
   demo?: string;
 }
@@ -21,35 +23,63 @@ export interface Proyecto
   {
   selector: 'app-proyectos',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule, CreateProyectComponent],
   templateUrl: './proyects.html',
   styleUrl: './proyects.css'
   }
 )
+
 export class Proyectos implements OnInit 
 {
   proyectos: Proyecto[] = [];
+  showModal = false; 
 
   constructor
   (
     private proyectsService: proyectService,
-    private cdr: ChangeDetectorRef 
+    private cdr: ChangeDetectorRef
   ) {}
 
   ngOnInit() 
   {
-    this.proyectsService.getproyects().subscribe
-    (
-      {
-      next: (data) => 
-      {
+    this.loadProyects();
+  }
+
+  loadProyects() 
+  {
+    this.proyectsService.getproyects().subscribe({
+      next: (data) => {
         this.proyectos = data;
-        this.cdr.detectChanges();  
+        this.cdr.detectChanges();
         console.log('Proyectos cargados:', data);
       },
       error: (error) => console.error('Error al cargar proyectos:', error),
       complete: () => console.info('Carga de proyectos completada')
-      }
-    );
+    });
+  }
+
+  openModal() 
+  {
+    this.showModal = true;
+  }
+
+  closeModal() 
+  {
+    this.showModal = false;
+  }
+
+  createProyect(proyectData: any) 
+  {
+    this.proyectsService.createProyect(proyectData).subscribe
+    ({
+      next: (newProyect) => 
+      {
+        this.proyectos = [...this.proyectos, newProyect];
+        this.cdr.detectChanges();
+        this.closeModal();
+        console.log('Proyecto creado:', newProyect);
+      },
+      error: (error) => console.error('Error al crear proyecto:', error)
+    });
   }
 }
